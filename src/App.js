dc20bc5bc65c32de58a7ed2a838d01b25aa48042
gamexan <curtosigabriel@gmail.com>
2022-01-22 03:41:34 -0300

    el navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavBar from './navBar/components/navBar/NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <h1>Hola</h1>
+      
     </div>
   );
 }
